4892ab0732f1aac0a5f8f48314f49ce603ae432b
TrinhDinhHoang29 <behoangloduoc@gmail.com>
2024-06-22 18:15:08 +0700

    a

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ app.set("view engine","pug");
 app.use(express.static(`${__dirname}/public`));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+app.use('/tinymce', express.static(path.join(__dirname, '../node_modules', 'tinymce')));
 
 routerClient(app);
 routerAdmin(app);
